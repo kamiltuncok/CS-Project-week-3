f19d2cd0f6a1058bc9783829df9733acfe0df0be
tivily <107156729+tivily@users.noreply.github.com>
2022-12-12 00:42:23 +0300

    end

---
+++ Post and Get/routes/logs.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const Log =require('../modules/log.js');
+
+
+const router=express.Router();
+
+router.post('/', (req,res) =>{
+    const post= new Log({
+      action:req.body.action,
+      type:req.body.type,
+      amount:req.body.amount,
+      date:req.body.date
+  
+    });
+  
+    post.save()
+        .then(data => {
+           res.json(data);
+        })
+        .catch(err => {
+          res.json({message:err});
+       });
+  });
+
+  router.get('/getall', async (req,res) => {
+    try {
+      const logs = await Log.find();
+      res.json(logs);
+    } catch (error) {
+      res.status(500).json({message:error.message});
+    }
+  })
+  
+  module.exports=router;
+  
